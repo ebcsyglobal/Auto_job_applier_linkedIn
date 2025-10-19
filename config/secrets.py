9331e5c594a6fcf8a4261696826580362e23a823
Copywriter | SEO | Web Developer <88982866+ebcsyglobal@users.noreply.github.com>
2025-10-19 01:55:26 -0500

    Enable AI usage and update API configurations

--- config/secrets.py
+++ config/secrets.py
@@ -23,7 +23,7 @@ password = "Rigatoni1*"           # Enter your password in the quotes
 
 ## Artificial Intelligence (Beta Not-Recommended)
 # Use AI
-use_AI = False                          # True or False, Note: True or False are case-sensitive
+use_AI = True                          # True or False, Note: True or False are case-sensitive
 '''
 Note: Set it as True only if you want to use AI, and If you either have a
 1. Local LLM model running on your local machine, with it's APIs exposed. Example softwares to achieve it are:
@@ -50,20 +50,20 @@ Note: Select your AI provider.
 
 
 # Your LLM url or other AI api url and port
-llm_api_url = "disabled"       # Examples: "https://api.openai.com/v1/", "http://127.0.0.1:1234/v1/", "http://localhost:1234/v1/", "https://api.deepseek.com", "https://api.deepseek.com/v1"
+llm_api_url = "https://api.openai.com/v1/"       # Examples: "https://api.openai.com/v1/", "http://127.0.0.1:1234/v1/", "http://localhost:1234/v1/", "https://api.deepseek.com", "https://api.deepseek.com/v1"
 '''
 Note: Don't forget to add / at the end of your url. You may not need this if you are using Gemini.
 '''
 
 # Your LLM API key or other AI API key 
-llm_api_key = "not-needed"              # Enter your API key in the quotes, make sure it's valid, if not will result in error.
+llm_api_key = "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"              # Enter your API key in the quotes, make sure it's valid, if not will result in error.
 '''
 Note: Leave it empty as "" or "not-needed" if not needed. Else will result in error!
 If you are using ollama, you MUST put "not-needed".
 '''
 
 # Your LLM model name or other AI model name
-llm_model = ""          # Examples: "gpt-3.5-turbo", "gpt-4o", "llama-3.2-3b-instruct", "qwen3:latest", "gemini-pro", "gemini-1.5-flash", "gemini-2.5-flash", "deepseek-llm:latest"
+llm_model = "gpt-4o-mini"          # Examples: "gpt-3.5-turbo", "gpt-4o", "llama-3.2-3b-instruct", "qwen3:latest", "gemini-pro", "gemini-1.5-flash", "gemini-2.5-flash", "deepseek-llm:latest"
 
 llm_spec = "openai"                # Examples: "openai", "openai-like", "openai-like-github", "openai-like-mistral"
 '''
